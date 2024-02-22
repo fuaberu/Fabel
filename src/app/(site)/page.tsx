57af03f5e05114b218b801e8bc4254320b1dacb8
fuaberu <86504230+fuaberu@users.noreply.github.com>
2024-02-22 14:01:23 -0300

    Fix typo

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -24,7 +24,7 @@ const Page = async () => {
 			<section className="relative flex h-[100svh] w-full flex-col items-center justify-center">
 				<div className="absolute bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
 
-				<p className="text-center">Manage your business, in one placee</p>
+				<p className="text-center">Manage your business, in one place</p>
 				<div className="bg-gradient-to-r from-primary to-secondary-foreground bg-clip-text text-transparent">
 					<h1 className="h-full text-center text-9xl font-bold md:text-[300px]">Fabel</h1>
 				</div>
